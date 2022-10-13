a261f981c3d6c45b3bbb525b52451a8e6f8626c4
sirdan <stanford303@gmail.com>
2022-10-13 15:20:28 -0700

    all tables has been added

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,10 @@ DROP TABLE IF EXISTS userClips CASCADE;
 DROP TABLE IF EXISTS usersComments CASCADE;
 DROP TABLE IF EXISTS comments CASCADE;
 DROP TABLE IF EXISTS votes CASCADE;
+DROP TABLE IF EXISTS communityFeedTable CASCADE;
+DROP TABLE IF EXISTS followers CASCADE;
+
+
 
  CREATE TABLE users (
     id bigint generated always as identity primary key,
@@ -16,8 +20,7 @@ DROP TABLE IF EXISTS votes CASCADE;
  );
 
 CREATE TABLE userClips (
-    id bigint generated always as identity,
-    clip_id varchar primary key,
+    id bigint generated always as identity primary key,
     clipLink varchar not null,
     users_id bigint,
     foreign key (users_id) references users(id)
@@ -25,7 +28,7 @@ CREATE TABLE userClips (
 
 CREATE TABLE usersComments (
     id bigint generated always as identity,
-    comment_id varchar not null,
+    comment_id bigint not null,
     users_id bigint primary key,
     foreign key (users_id) references users(id)
 );
@@ -33,10 +36,10 @@ CREATE TABLE usersComments (
 CREATE TABLE comments (
     id bigint generated always as identity primary key,
     commenter_id bigint not null,
-    clip_id varchar not null,
+    clip_id bigint not null,
     comments varchar not null,
     foreign key (commenter_id) references usersComments(users_id),
-    foreign key (clip_id) references userClips(clip_id)
+    foreign key (clip_id) references userClips(id)
 );
 
 CREATE TABLE votes (
@@ -47,6 +50,19 @@ CREATE TABLE votes (
     foreign key (commenter_id) references users(id)
 );
 
+CREATE TABLE communityFeedTable (
+    id bigint generated always as identity,
+    clip_id bigint not null,
+    users_id bigint not null,
+    foreign key (clip_id) references userClips(id)
+);
+
+CREATE TABLE followers (
+    id bigint generated always as identity,
+    users_id bigint not null,
+    follower_id bigint not null,
+    followed boolean
+);
 
 
 
